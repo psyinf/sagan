0208514c428d590fc0f452b8c694f23a8eadaaa5
psy_inf <sebastian@smesserschmidt.de>
2023-10-02 12:48:44 +0200

    yaml config loading

--- src/sandbox/main.cpp
+++ src/sandbox/main.cpp
@@ -1,11 +1,14 @@
 #include "SceneEntity.h"
 #include "SceneFactory.h"
 #include "ViewerCore.h"
+#include "yaml-cpp/yaml.h"
 
 int main(int argc, char** argv)
 {
     renderer::ViewerCore viewerCore;
     viewerCore.setup();
+
+    YAML::Node config = YAML::LoadFile("data/config/viewer.yaml");
 
     renderer::SceneFactory sceneFactory{viewerCore.getViewer()};
     vsg::GeometryInfo      geom_info;
